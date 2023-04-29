2c8c2557bff4bc8f982143499c1d75d54a65eb7b
myrdstom <nserekopaul@gmail.com>
2023-04-29 18:57:15 +0300

    add validation to article creation

--- src/routes/api-v1/articles.route.js
+++ src/routes/api-v1/articles.route.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import verifiedToken from '../../middleware/auth';
 import asyncMiddleware from '../../middleware/async';
 import ArticlesController from '../../controllers/articles';
+import validate from "../../middleware/validate";
 const router = Router();
 
 router.get('/', ArticlesController.getAllArticles);
-router.post('/', verifiedToken, ArticlesController.postAnArticle);
+router.post('/', verifiedToken,   validate.articles,ArticlesController.postAnArticle);
 
 router.get('/:id', asyncMiddleware(ArticlesController.getArticleWithId));
 
